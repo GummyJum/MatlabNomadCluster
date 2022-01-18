2a23792eedd64df886244bb144533d01caf7a651
Amir <Amir.goldental@gmail.com>
2022-01-18 21:42:45 +0200

    get all tasks if no task_id is given

--- +mnc/task_status.m
+++ +mnc/task_status.m
@@ -1,4 +1,8 @@
 function status = task_status(task_id)
-status = webread([mnc.config('nomad_url') '/job/' task_id]);
+if ~exist('task_id', 'var')
+    status = struct2table(webread([mnc.config('nomad_url') '/jobs']));
+else
+    status = webread([mnc.config('nomad_url') '/job/' task_id]);
 end
 
+end
